acdbfa5dfc1151a40f162c902b161b9ee4ed0ddc
Erwin de Gelder <erwin.degelder@tno.nl>
2024-06-03 09:17:19 +0200

    Achieve 100 percent coverage for vertex.py.

--- tests/test_vertex.py
+++ tests/test_vertex.py
@@ -1,6 +1,23 @@
-from traffic_scene_renderer import Vertex
+"""Scripts for testing all functionalities from vertex.py.
+
+Author(s): Erwin de Gelder
+"""
+
+from traffic_scene_renderer import Vertex, VertexOptions
 
 
-def test_dummy():
+def test_vertex_creation() -> None:
     vertex = Vertex(0, 0, 0)
-    pass
+    assert str(vertex) == "Vertex[ID=0, x=0.00, y=0.00]"
+
+
+def test_vertex_creation_with_latlon() -> None:
+    vertex_options = VertexOptions(latlon=True)
+    # Location of TNO Automotive :)
+    vertex = Vertex(0, 51.474457, 5.623897, options=vertex_options)
+    assert str(vertex) == "Vertex[ID=0, x=682219.44, y=5705853.69]"
+
+
+def test_vertex_get_xy() -> None:
+    vertex = Vertex(0, 1, 2)
+    assert vertex.get_xy() == [1, 2]
