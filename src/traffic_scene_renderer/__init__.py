fe51735100ee2cf8a893cba362d35490e093b396
Erwin de Gelder <erwin.degelder@tno.nl>
2024-07-28 10:51:32 +0200

    Adding road network.

--- src/traffic_scene_renderer/__init__.py
+++ src/traffic_scene_renderer/__init__.py
@@ -11,6 +11,13 @@ from .crossing import Crossing, CrossingOptions
 from .letters import Letter, LetterOptions, Letters, LettersOptions
 from .options import FrozenOptionsError, Options, UnknownOptionError
 from .path_follower import PathFollower
+from .road_network import (
+    CrossingInfo,
+    RoadNetwork,
+    RoadNetworkNoAxesError,
+    RoadNetworkOptions,
+    StopLineOptions,
+)
 from .static_objects import (
     Building,
     BuildingOptions,
